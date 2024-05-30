9b88b64e171f08566af2854060ebc241869922d8
222112038 <falanarofako29@gmail.com>
2024-05-30 20:55:32 +0700

    add beranda atas fragment

---
+++ src/pages/beranda.jsx
@@ -0,0 +1,10 @@
+import BerandaAtas from "../components/Fragments/BerandaAtas";
+
+export default function Beranda() {
+
+    return (
+        <div>
+            <BerandaAtas />
+        </div>
+    );
+}
