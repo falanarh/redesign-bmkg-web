7987d4a72e33d2ffb3afa7b83c686be4abb23195
222112038 <falanarofako29@gmail.com>
2024-05-30 21:42:13 +0700

    fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Footer } from "antd/es/layout/layout";
 import EarlyWarning from "./components/Elements/EarlyWarning";
 import NavbarSection from "./components/Layouts/NavbarSection";
 import SideFooter from "./components/Elements/SideFooter";
+import Beranda from "./pages/beranda";
 
 export default function App() {
   return (
     <div className="fixed top-0 left-0 right-0">
       <NavbarSection />
       <EarlyWarning />
+      {/* <Beranda /> */}
       <SideFooter />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-    <div style={{ height: "1000px", overflow: "auto" }}>
+    <div style={{ overflow: "auto" }}>
         <App />
       </div>
     </NextUIProvider>
