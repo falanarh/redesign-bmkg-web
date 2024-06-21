7aeab2ffc42f6e27f63cb40f240aa24040a5d4f1
gresss1002 <102232893+gresss1002@users.noreply.github.com>
2024-06-22 02:59:07 +0700

    little in iklimanomali

--- src/components/Fragments/IklimAnomaliSuhuUdara/index.jsx
+++ src/components/Fragments/IklimAnomaliSuhuUdara/index.jsx
@@ -23,16 +23,6 @@ const onChange = (key) => {
     return (
       <ContentSection
         title="Anomali Suhu Udara"
-        // description={
-        //   <p className="text-[14px] font-pt-sans">
-        //     Prakiraan berbasis dampak atau Impact-Based Forecast (IBF) merupakan
-        //     informasi prakiraan yang sudah memperhitungkan potensi dampak yang akan
-        //     terjadi akibat dari cuaca. Dalam sistem IBF juga disajikan rekomendasi
-        //     respon atau langkah yang harus dilakukan oleh stakeholder/user atau
-        //     masyarakat terkait dampak dari dinamika cuaca tersebut. Untuk informasi
-        //     selengkapnya kunjungi <Link href="https://signature.bmkg.go.id" target="_blank" className="text-active font-semibold" >website</Link> berikut.
-        //   </p>
-        // }
       >
         <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
       </ContentSection>
