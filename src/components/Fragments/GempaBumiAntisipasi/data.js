665966a2c34b7507a86d93a93b55776256f23f30
gresss1002 <102232893+gresss1002@users.noreply.github.com>
2024-06-23 14:55:20 +0700

    add GBAntisipasi

--- src/components/Fragments/GempaBumiAntisipasi/data.js
+++ src/components/Fragments/GempaBumiAntisipasi/data.js
@@ -1,111 +1,173 @@
-export const data=[
+export const data1=[
     {
         id: 1,
         image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi01.png',
-        title:'Skala I MMI',
+        title:'A. Kunci Utama adalah',
         description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
+            'Mengenali apa yang disebut gempabumi;',
+            'Pastikan bahwa struktur dan letak rumah Anda dapat terhindar dari bahaya yang disebabkan oleh gempabumi (longsor, liquefaction dll);',
+            'Mengevaluasi dan merenovasi ulang struktur bangunan Anda agar terhindar dari bahaya gempabumi.',
         ],
     },
     {
         id: 2,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi2.png',
-        title:'Skala II MMI',
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi02.png',
+        title:'B. Kenali Lingkungan Tempat Anda Bekerja',
         description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
+            'Perhatikan letak pintu, lift serta tangga darurat, apabila terjadi gempabumi, sudah mengetahui tempat paling aman untuk berlindung;',
+            'Belajar melakukan P3K;',
+            'Belajar menggunakan alat pemadam kebakaran;',
+            'Catat nomor telepon penting yang dapat dihubungi pada saat terjadi gempabumi.',
         ],
     },
     {
         id: 3,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi3.png',
-        title:'Skala III MMI',
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi03.png',
+        title:'C. Persiapan Rutin pada tempat Anda bekerja dan tinggal',
         description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
+            'Perabotan (lemari, cabinet, dll) diatur menempel pada dinding (dipaku, diikat, dll) untuk menghindari jatuh, roboh, bergeser pada saat terjadi gempabumi.',
+            'Simpan bahan yang mudah terbakar pada tempat yang tidak mudah pecah agar terhindar dari kebakaran.',
+            'Selalu mematikan air, gas dan listrik apabila tidak sedang digunakan.',
         ],
     },
     {
         id: 4,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi4.png',
-        title:'Skala IV MMI',
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi04.png',
+        title:'D. Penyebab celaka yang paling banyak pada saat gempabumi adalah akibat kejatuhan material',
         description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
+            'Atur benda yang berat sedapat mungkin berada pada bagian bawah',
+            'Cek kestabilan benda yang tergantung yang dapat jatuh pada saat gempabumi terjadi (misalnya lampu dll).'
         ],
     },
     {
         id: 5,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi5.png',
-        title:'Skala V MMI',
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi05.png',
+        title:'E. Alat yang harus ada di setiap tempat',
         description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
+            'Kotak P3K;',
+            'Senter/lampu baterai;',
+            'Radio;',
+            'Makanan suplemen dan air.'
+        ],
+    },
+];
+
+export const data2=[
+    {
+        id: 1,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi06.png',
+        title:'A. Jika Anda berada di dalam bangunan',
+        description:[
+            'Lindungi badan dan kepala Anda dari reruntuhan bangunan dengan bersembunyi di bawah meja dll;',
+            'Cari tempat yang paling aman dari reruntuhan dan goncangan;',
+            'Lari ke luar apabila masih dapat dilakukan',
+        ],
+    },
+    {
+        id: 2,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi07.png',
+        title:'B. Jika berada di luar bangunan atau area terbuka',
+        description:[
+            'Menghindari dari bangunan yang ada di sekitar Anda seperti gedung, tiang listrik, pohon, dll',
+            'Perhatikan tempat Anda berpijak, hindari apabila terjadi rekahan tanah',
+        ],
+    },
+    {
+        id: 3,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi08.png',
+        title:'C. Jika Anda sedang mengendarai mobil',
+        description:[
+            'Keluar, turun dan menjauh dari mobil hindari jika terjadi pergeseran atau kebakaran;',
+            'Lakukan point B.',
+        ],
+    },
+    {
+        id: 4,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi09.png',
+        title:'D. Jika Anda tinggal atau berada di pantai',
+        description:[
+            'Jauhi pantai untuk menghindari bahaya tsunami.',
+            ''
+        ],
+    },
+    {
+        id: 5,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi10.png',
+        title:'E. Jika Anda tinggal di daerah pegunungan',
+        description:[
+            'Apabila terjadi gempabumi hindari daerah yang mungkin terjadi longsoran.',
+            '',
+        ],
+    },
+]
+
+export const data3=[
+    {
+        id: 1,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi11.png',
+        title:'A. Jika Anda berada di dalam bangunan',
+        description:[
+            'Keluar dari bangunan tersebut dengan tertib;',
+            'Jangan menggunakan tangga berjalan atau lift, gunakan tangga biasa;',
+            'Periksa apa ada yang terluka, lakukan P3K;',
+            'Telepon atau mintalah pertolongan apabila terjadi luka parah pada Anda atau sekitar Anda.'
+        ],
+    },
+    {
+        id: 2,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi12.png',
+        title:'B. Periksa lingkungan sekitar Anda',
+        description:[
+            'Periksa apabila terjadi kebakaran.',
+            'Periksa apabila terjadi kebocoran gas.',
+            'Periksa apabila terjadi hubungan arus pendek listrik.',
+            'Periksa aliran dan pipa air.',
+            'Periksa apabila ada hal-hal yang membahayakan (mematikan listrik, tidak menyalakan api dll)'
+        ],
+    },
+    {
+        id: 3,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi13.png',
+        title:'C. Jangan mamasuki bangunan yang sudah terkena gempa',
+        description:[
+            'Karena kemungkinan masih terdapat reruntuhan.',
+            '',
+        ],
+    },
+    {
+        id: 4,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi14.png',
+        title:'D. Jangan berjalan di daerah sekitar gempa',
+        description:[
+            'Kemungkinan terjadi bahaya susulan masih ada.',
+            ''
+        ],
+    },
+    {
+        id: 5,
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi15.png',
+        title:'E. Mendengarkan informasi.',
+        description:[
+            'Dengarkan informasi mengenai gempabumi dari radio (apabila terjadi gempa susulan).',
+            'Jangan mudah terpancing oleh isu atau berita yang tidak jelas sumbernya.',
         ],
     },
     {
         id: 6,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi6.png',
-        title:'Skala VI MMI',
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi16.png',
+        title:'F. Mengisi angket yang diberikan oleh instansi terkait untuk mengetahui seberapa besar kerusakan yang terjadi',
         description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
+            null
         ],
     },
     {
         id: 7,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi7.png',
-        title:'Skala VII MMI',
+        image: 'https://bmkg.go.id/asset/img/gempabumi/antisipasi17.png',
+        title:"G. Jangan panik dan jangan lupa selalu berdo'a kepada Tuhan YME demi keamanan dan keselamatan kita semuanya.",
         description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
+            null
         ],
     },
-    {
-        id: 8,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi8.png',
-        title:'Skala VIII MMI',
-        description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
-        ],
-    },
-    {
-        id: 9,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi9.png',
-        title:'Skala IX MMI',
-        description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
-        ],
-    },
-    {
-        id: 10,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi10.png',
-        title:'Skala X MMI',
-        description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
-        ],
-    },
-    {
-        id: 11,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi11.png',
-        title:'Skala XI MMI',
-        description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
-        ],
-    },
-    {
-        id: 12,
-        image: 'https://bmkg.go.id/asset/img/gempabumi/mmi12.png',
-        title:'Skala XII MMI',
-        description:[
-            'Getaran tidak dirasakan kecuali dalam keadaan luarbiasa oleh beberapa orang',
-            'Getaran dirasakan oleh beberapa orang, benda-benda ringan yang digantung bergoyang.'
-        ],
-    },
-];
+]
+
 
--- src/components/Fragments/GempaBumiAntisipasi/index.jsx
+++ src/components/Fragments/GempaBumiAntisipasi/index.jsx
@@ -1,17 +1,22 @@
-import { Card } from "antd";
-import { data } from './data';
-import CustomCard from "../../Elements/CustomCard";
+import { data1, data2, data3 } from './data';
 import ContentSection from "../../Layouts/ContentSection";
 import AntsipasiGBCard from "../../Elements/AntisipasiGBCard";
 
 const GBAntisipasi = () => {
     return (
         <ContentSection title="Antisipasi Gempa Bumi" description="Gempa bumi dapat terjadi kapan pun dan dimana pun tanpa mengenal tempat dan waktu. Sebagai salah satu negara yang rawan akan bencana ini, kita sebagai warga perlu memiliki pembekalan diri mengenai cara menyelamatkan diri. Hal-hal apa yang perlu dilakukan sebelum, sesudah, dan setelah gempa bumi?">
-           {data.map((item, index) => (
+           <p className="font-bold text-xl text-active mb-5">Sebelum Terjadi Gempa Bumi</p>
+           {data1.map((item, index) => (
             <AntsipasiGBCard key={index} item={item} />
-            
            ))} 
-
+            <p className="font-bold text-xl text-active mb-5">Saat Terjadi Gempa Bumi</p>
+           {data2.map((item, index) => (
+            <AntsipasiGBCard key={index} item={item} />
+           ))} 
+           <p className="font-bold text-xl text-active mb-5">Setelah Terjadi Gempa Bumi</p>
+           {data3.map((item, index) => (
+            <AntsipasiGBCard key={index} item={item} />
+           ))} 
         </ContentSection>
     );
 };
